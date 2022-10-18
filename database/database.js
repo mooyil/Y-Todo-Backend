fc4c2c1761888b28a1500190229e0bbe3ae17f3f
MO <m.yildiz.26@outlook.de>
2022-10-18 17:31:08 +0200

    Datenbank mit Testdaten erstellen

--- database/database.js
+++ database/database.js
@@ -8,18 +8,50 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
   } else {
     console.log("Connected to the SQLite database.");
     db.run(
-      `CREATE TABLE todos (
+      ` CREATE TABLE users (
+          email text PRIMARY KEY not NULL,
+          password text NOT NULL,
+          userConfig text
+          )`,
+      (err) => {
+        if (err) {
+        } else {
+          db.run(
+            `
+          CREATE TABLE todos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             content text, 
             tab text,
-            date datetime
-            )`,
-      (err) => {
-        if (err) {
-          console.log("")
-        } 
+            date datetime,
+        userEmail text, 
+    FOREIGN KEY(userEmail) REFERENCES users(email)
+    
+        )`,
+
+            (err) => {
+              if (err) {
+              } else {
+                db.run(
+                  `INSERT INTO users (email, password, userConfig) VALUES ( "muhammed@outlook.de", "muhammed123", "")`
+                );
+                db.run(
+                  `INSERT INTO users (email, password, userConfig) VALUES ( "enes@outlook.de", "enes123", "")`
+                );
+                db.run(
+                  `INSERT INTO todos (content, tab, date, useremail) VALUES( "putzen", "mainListe", "2022-10-18T13:22", "muhammed@outlook.de");`
+                );
+                db.run(
+                  `INSERT INTO todos (content, tab, date, useremail) VALUES( "putzen", "mainListe", "2022-10-18T13:22", "enes@outlook.de");`
+                );
+                db.run(
+                  `INSERT INTO todos (content, tab, date, useremail) VALUES( "putzen", "mainListe", "2022-10-18T13:22", "enes@outlook.de");`
+                );
+              }
+            }
+          );
+        }
       }
-    )
+    );
   }
 });
 
